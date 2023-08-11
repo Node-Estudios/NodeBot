46189f86cee76653812dc0806aea04a8e7279c45
eliyya <tecno_eli@hotmail.com>
2023-08-10 18:14:10 -0600

    fix autocomplete play

--- src/slash/autocomplete/play/play.ts
+++ src/slash/autocomplete/play/play.ts
@@ -1,7 +1,8 @@
 import { AutocompleteInteraction } from 'discord.js'
-import Client from '../../../structures/Client'
-import logger from '#utils/logger.js'
+// import Client from '../../../structures/Client'
+// import logger from '#utils/logger.js'
 import Autocomplete from '#structures/Autocomplete.js'
+import yasha from 'yasha'
 
 export default class Repeat extends Autocomplete {
     constructor () {
@@ -9,16 +10,20 @@ export default class Repeat extends Autocomplete {
     }
 
     override async run (interaction: AutocompleteInteraction) {
-        try {
-            const client = interaction.client as Client
-            if (!interaction.options.getFocused()) return
-            const result = await (await client.music.youtubei).music.getSearchSuggestions(interaction.options.getFocused())
+        const query = interaction.options.getFocused()
+        const search = await yasha.Source.Youtube.search(query)
+        if (search.length > 25) search.length = 25
+        interaction.respond(search.map(r => ({ name: r.title ?? '', value: r.author ?? '' })))
+        // try {
+        //     const client = interaction.client as Client
+        //     if (!interaction.options.getFocused()) return
+        //     const result = await (await client.music.youtubei).music.getSearchSuggestions(interaction.options.getFocused())
 
-            // TODO: Remove this any
-            interaction.respond(result.map((choice: any) => ({ name: choice.suggestion?.text, value: choice.suggestion?.text })))
-        } catch (e) {
-            logger.error(e)
-        }
-        return true
+        //     // TODO: Remove this any
+        //     interaction.respond(result.map((choice: any) => ({ name: choice.suggestion?.text, value: choice.suggestion?.text })))
+        // } catch (e) {
+        //     logger.error(e)
+        // }
+        // return true
     }
 }
