b0b6c1df9373a883e2f8bde790552a7fa8353d8b
eliyya <tecno_eli@hotmail.com>
2023-08-13 16:58:03 -0600

    parse embed fields and descriptions

--- src/structures/EmbedBuilder.ts
+++ src/structures/EmbedBuilder.ts
@@ -3,25 +3,40 @@ import { APIEmbedField, EmbedBuilder as EmbedBuilderBase, RestOrArray, normalize
 export default class EmbedBuilder extends EmbedBuilderBase {
     override setFields (...fields: RestOrArray<APIEmbedField>): this {
         fields = normalizeArray(fields)
+        const parsedFields: APIEmbedField[] = []
         for (const field of fields) {
+            if (!field.value || !field.name) continue
+
             if (field.name.length > 256)
                 field.name = field.name.slice(0, 253) + '...'
 
             if (field.value.length > 1024)
                 field.value = field.value.slice(0, 1020) + '...'
+            parsedFields.push(field)
         }
-        return super.setFields(fields)
+        return super.setFields(parsedFields)
     }
 
     override addFields (...fields: RestOrArray<APIEmbedField>): this {
         fields = normalizeArray(fields)
+        const parsedFields: APIEmbedField[] = []
         for (const field of fields) {
+            if (!field.value || !field.name) continue
+
             if (field.name.length > 256)
                 field.name = field.name.slice(0, 253) + '...'
 
             if (field.value.length > 1024)
                 field.value = field.value.slice(0, 1020) + '...'
+            parsedFields.push(field)
         }
-        return super.addFields(fields)
+        return super.setFields(parsedFields)
+    }
+
+    override setDescription (description: string | null): this {
+        if (!description) return this
+        if (description && description.length > 4096)
+            description = description.slice(0, 4092) + '...'
+        return super.setDescription(description)
     }
 }
