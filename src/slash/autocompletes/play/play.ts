0f53437af45d458203de5bbcc2a5b063a07d9d2c
eliyya <tecno_eli@hotmail.com>
2023-08-31 22:04:10 -0600

    add query as first option in search

--- src/slash/autocompletes/play/play.ts
+++ src/slash/autocompletes/play/play.ts
@@ -14,16 +14,19 @@ export default class Repeat extends Autocomplete {
         try {
             const query = interaction.options.getFocused()
             const search = await yasha.Source.Youtube.search(query)
-            if (search.length > 25) search.length = 25
+            if (search.length > 25) search.length = 24
             if (!this.canProced(interaction.user.id, interaction.id)) return false
-            await interaction.respond(search.map(r => {
+            await interaction.respond([{
+                name: query,
+                value: query,
+            }, ...search.map(r => {
                 const title = r.title ?? ''
                 const url = r.url ?? ''
                 return {
                     name: title.length > 100 ? title.slice(0, 95) + '...' : title,
                     value: url.length > 100 ? url.slice(0, 95) + '...' : url,
                 }
-            })).catch(logger.error)
+            })]).catch(logger.error)
             return true
         } catch (error) {
             logger.error(error)
