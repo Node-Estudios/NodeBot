d31855f977f673eea7237b1b5d6b5ccd350bbb8d
eliyya <tecno_eli@hotmail.com>
2025-04-21 13:53:24 -0600

    create models db

--- src/utils/db/retrieveUserLang.ts
+++ src/utils/db/retrieveUserLang.ts
@@ -5,32 +5,46 @@ async function createUserInDB (input: User) {
     return await Usermodel.create(input)
 }
 
-export async function getUserFromDB (input: GuildMember | DiscordUser | string): Promise<User | null> {
+export async function getUserFromDB (
+    input: GuildMember | DiscordUser | string,
+): Promise<User | null> {
     // logger.debug('getUserFromDB exeuted with input: ' + input)
     switch (true) {
         case input instanceof GuildMember:
-            return await Usermodel.findOneAndUpdate({ id: (input as GuildMember).id }, async (_: any, user: User) => {
-                if (user) return user
-                return await createUserInDB({
-                    id: (input as GuildMember).id,
-                    executedCommands: 0,
-                    roles: { Developer: { enabled: false }, Tester: { enabled: false } },
-                })
-            })
-        case input instanceof String:
-            return await Usermodel.findOneAndUpdate({ id: input as String }, async (error: any, user: User) => {
-                if (user) {
-                    return user
-                }
-                if (!user) {
+            return await Usermodel.findOneAndUpdate(
+                { id: input.id },
+                async (_: any, user: User) => {
+                    if (user) return user
                     return await createUserInDB({
-                        id: (input as GuildMember).id,
+                        id: input.id,
                         executedCommands: 0,
-                        roles: { Developer: { enabled: false }, Tester: { enabled: false } },
+                        roles: {
+                            Developer: { enabled: false },
+                            Tester: { enabled: false },
+                        },
                     })
-                }
-                return error ? undefined : user
-            })
+                },
+            )
+        case input instanceof String:
+            return await Usermodel.findOneAndUpdate(
+                { id: input as String },
+                async (error: any, user: User) => {
+                    if (user)
+                        return user
+
+                    if (!user)
+                        return await createUserInDB({
+                            id: (input as GuildMember).id,
+                            executedCommands: 0,
+                            roles: {
+                                Developer: { enabled: false },
+                                Tester: { enabled: false },
+                            },
+                        })
+
+                    return error ? undefined : user
+                },
+            )
         default:
             return null
     }
