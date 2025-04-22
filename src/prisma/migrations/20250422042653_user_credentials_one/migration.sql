2d5c6219d77ab868eecd31a4a140f983f98ac004
eliyya <tecno_eli@hotmail.com>
2025-04-22 14:53:23 -0600

    remove generated and create command

--- src/prisma/migrations/20250422042653_user_credentials_one/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[user_id]` on the table `users_credentials` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "users_credentials_user_id_key" ON "users_credentials"("user_id");
