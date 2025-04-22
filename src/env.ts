bd72e2c75297f07b01500f5aa590a68122f8bae1
eliyya <tecno_eli@hotmail.com>
2025-04-22 15:03:42 -0600

    regex changued

--- src/env.ts
+++ src/env.ts
@@ -2,7 +2,7 @@ process.loadEnvFile()
 const { ENCRYPTION_KEY = '' } = process.env
 
 if (!ENCRYPTION_KEY) throw new Error('ENCRYPTION_KEY in .env is required')
-if (!/^[0-9a-fA-F]{64}$/i.test(ENCRYPTION_KEY))
+if (!/^[0-9a-f]{64}$/i.test(ENCRYPTION_KEY))
     throw new Error(
         'ENCRYPTION_KEY in .env must be a valid 64 characters hexadecimal string',
     )
